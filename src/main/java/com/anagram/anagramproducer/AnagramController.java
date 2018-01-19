64d34c1e14ed48cb4603ae94c02ec41b44534efd
sbalajims <sbalajims@gmail.com>
2018-01-19 09:03:51 +0000

    Appilication which generates Anagrams for a given word

---
+++ src/main/java/com/anagram/anagramproducer/AnagramController.java
@@ -0,0 +1,91 @@
+package com.anagram.anagramproducer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AnagramController {
+
+	@RequestMapping(value = "/{word}", method = RequestMethod.GET, produces = "application/json")
+	public List<String> getAnagrams(@PathVariable String word) throws IOException {
+		List<String> anagrams = new ArrayList<String>();
+		// Reads the input file
+		Set<String> fileContentSet = readFile();
+
+		// Generates all possible combinations of a given word
+		List<String> permuteSet = findAllPermutations(word);
+
+		// Checks if the dictionary collection contains a perumuted value
+		// Prints the value if it is available in the wordlist.txt
+		for (String permutedValue : permuteSet) {
+			if (fileContentSet.contains(permutedValue)) {
+				anagrams.add(permutedValue);
+			}
+		}
+		
+		return anagrams;
+	}
+
+	/**
+	 * Reads the contents of the file from the given URL and collects it into a SET
+	 * object
+	 * 
+	 * @return
+	 * @throws IOException
+	 */
+	private Set<String> readFile() throws IOException {
+		Set<String> fileContentSet = new HashSet<String>();
+		System.out.println("Started reading the input file...");
+		URL wordList = new URL("http://static.abscond.org/wordlist.txt");
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(wordList.openStream()))) {
+			fileContentSet = br.lines().collect(Collectors.toSet());
+		} catch (IOException e) {
+			System.out.println("Exception while reading a file. The exception is : ");
+			e.printStackTrace();
+		}
+		System.out.println("Completed reading the input file");
+		return fileContentSet;
+
+	}
+
+	/**
+	 * Generates all the possible combinations of a given word
+	 * 
+	 * @param word
+	 * @return
+	 */
+	public List<String> findAllPermutations(String word) {
+		if (word == null) {
+			throw new NullPointerException();
+		}
+		if (word.length() <= 1) {
+			return Arrays.asList(word);
+		}
+
+		List<String> permutations = new ArrayList<>();
+		for (String permutation : findAllPermutations(word.substring(1))) {
+			char ch = word.charAt(0);
+			for (int i = 0; i <= permutation.length(); i++) {
+				String prefix = permutation.substring(0, i);
+				String suffix = permutation.substring(i);
+				permutations.add(prefix + ch + suffix);
+			}
+		}
+		return permutations;
+	}
+}
